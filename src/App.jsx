33cbae5ea8f6c976e52cf076632ced35ac74da10
Christopher Giroir <kelsin@valefor.com>
2025-06-11 12:55:55 -0700

    fix: don't filter games out for 1 player selected

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
           if (hidePlayed && getPlayedCount(id) > 0) {
             return;
           }
-          if (num_players > 0 && (num_players < min || num_players > max)) {
+          if (num_players > 1 && (num_players < min || num_players > max)) {
             return;
           }
 
